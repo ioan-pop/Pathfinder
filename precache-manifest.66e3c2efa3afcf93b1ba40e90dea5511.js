31aa73bc416d7afda9f266bd270b5d9f78b79841
Ioan Pop <ioan@verified.best>
2020-09-10 02:21:36 -0400

    Updates

---
+++ precache-manifest.66e3c2efa3afcf93b1ba40e90dea5511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c52d243c44d001db1f2b537505b6165",
+    "url": "/Prototype/index.html"
+  },
+  {
+    "revision": "95c19008b617af670838",
+    "url": "/Prototype/static/css/main.437da0a4.chunk.css"
+  },
+  {
+    "revision": "098e87b3f27b0b8cd5b6",
+    "url": "/Prototype/static/js/2.be031360.chunk.js"
+  },
+  {
+    "revision": "8f249d5627159228beaddbb193987905",
+    "url": "/Prototype/static/js/2.be031360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c19008b617af670838",
+    "url": "/Prototype/static/js/main.41e91a1f.chunk.js"
+  },
+  {
+    "revision": "648c221d4ed12da9615a",
+    "url": "/Prototype/static/js/runtime-main.872e082a.js"
+  }
+]);
